c28b2d006db1a3a1d56ea9191b6d01573f1800d8
Tim Quimbo <tquimbo1@gmail.com>
2021-11-02 22:48:37 -0400

    commit tree 6ce46586dcb27ea0d5d2020bc90e6ffa84403409

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    def new
+        # nothing to do here!
+    end
+
+    def create
+        if params[:name].present?
+        session[:name] = params[:name]
+        redirect_to '/'
+        else
+            redirect_to '/login'
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+
+end
